e08aa332094c3a357ad365c5fa3cd15dd3dff43f
yuna <yjeong1217@gmail.com>
2024-07-21 02:33:52 +0900

    Stage 생성

---
+++ Assets/02_Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform transform;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if ()
+    }
+
+
+}
